ed3647d0d3d9ea42203fcddbf745ab0be1c66b68
YousufEDS <yousuf@edsglobal.com>
2026-02-06 15:43:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 # Pushing the code into repository.
-<<<<<<< HEAD
 
 # from vs code to push in github.
-=======
 # this line is written from github.
->>>>>>> 01db589f7f05a3f2d1264bc6836f318820092e55
